428d135bd117eac6b77e36eb0f9733d3e1d86bcf
Robert Rosborg <rosborg.robert@gmail.com>
2023-10-17 18:16:34 +0200

    Expanded authentication repository

--- lib/src/repositories/authentication_repository.dart
+++ lib/src/repositories/authentication_repository.dart
@@ -1,12 +1,66 @@
 import 'package:firedart/firedart.dart';
+import 'package:firedart/auth/user_gateway.dart' as user_gateway;
 import 'package:meta/meta.dart';
+import 'package:fpdart/fpdart.dart';
+import '../models/user.dart';
 
 @immutable
-final class AuthenticationRepository {
+class AuthenticationRepository {
   final FirebaseAuth auth; // pass it in so it can be mocked.
 
   AuthenticationRepository({required this.auth});
 
-    // TODO: Create methods for registration and login using Firebase Authentication.
+  Future<Option<User>> registerUser({required String email, required String password, required String userName}) async {
+    final user_gateway.User user;
+    try {
+      await auth.signUp(email, password);
+    } catch(e) {
+      return Option<User>.none();
+    }
 
+    try {
+      await auth.updateProfile(displayName: userName);
+    } catch(e) {
+      return Option<User>.none();
+    }
+
+    try {
+      user = await auth.getUser();
+    } catch(e) {
+      return Option<User>.none();
+    }
+
+    return Option<User>.of(
+        User(
+          id: user.id,
+          name: user.displayName != null ? user.displayName! : "",
+          email: user.email != null ? user.email! : "",
+        )
+    );
+  }
+
+  Future<Option<User>> loginUser({required String email, required String password}) async {
+    user_gateway.User user;
+    try {
+      user = await auth.signIn(email, password);
+    } catch(e) {
+      return Option<User>.none();
+    }
+
+    return Option<User>.of(
+        User(
+          id: user.id,
+          name: user.displayName != null ? user.displayName! : "",
+          email: user.email != null ? user.email! : "",
+        )
+    );
+  }
+
+  void logoutUser() {
+    auth.signOut();
+  }
+
+  Future<void> deleteUser() async {
+    await auth.deleteAccount();
+  }
 }
