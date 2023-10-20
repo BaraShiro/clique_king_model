2d2f6f99616e777be3fbe5843a51ee9cdfbb0d6e
Robert Rosborg <rosborg.robert@gmail.com>
2023-10-20 21:17:52 +0200

    Added named constructors to User

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -1,9 +1,10 @@
 import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firedart/auth/user_gateway.dart' as auth;
 
 @immutable
 final class User extends Equatable{
-  final String id; // TODO: probably should map to Firebase Authentication id.
+  final String id;
   final String name;
   final String email;
 
@@ -12,6 +13,26 @@ final class User extends Equatable{
     required this.name,
     required this.email,
   });
+
+  User.fromAuthUser(auth.User user):
+    id = user.id,
+    name = user.displayName ?? "",
+    email = user.email ?? "";
+
+  User.fromMap(Map<String, dynamic> map):
+    id = map['id'],
+    name = map['name'],
+    email = map['email'];
+
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'name': name,
+    'email': email,
+  };
+
+  User updateName(String newName) {
+    return User(id: id, name: newName, email: email);
+  }
 
   @override
   List<Object?> get props => [id, name, email];
