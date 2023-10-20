286a811149506008427d6ec5187c1c29b312805a
Robert Rosborg <rosborg.robert@gmail.com>
2023-10-20 21:59:27 +0200

    Small fix in user repository

--- lib/src/repositories/user_repository.dart
+++ lib/src/repositories/user_repository.dart
@@ -16,9 +16,9 @@ class UserRepository {
     try {
       await store.collection("users").document(user.id).create(user.toMap());
     } catch (e) {
-      return Option<RepositoryError>.of(RepositoryError(errorObject: e));
+      return Option.of(RepositoryError(errorObject: e));
     }
-    return Option<RepositoryError>.none();
+    return Option.none();
   }
 
   Future<Either<RepositoryError, User>> readUser({ required UserId id}) async {
@@ -35,18 +35,18 @@ class UserRepository {
     try {
       await store.collection("users").document(user.id).update(updatedUser.toMap());
     } catch (e) {
-      return Option<RepositoryError>.of(RepositoryError(errorObject: e));
+      return Option.of(RepositoryError(errorObject: e));
     }
-    return Option<RepositoryError>.none();
+    return Option.none();
   }
 
   Future<Option<RepositoryError>> deleteUSer({required UserId id}) async {
     try {
       await store.collection("users").document(id).delete();
     } catch (e) {
-      return Option<RepositoryError>.of(RepositoryError(errorObject: e));
+      return Option.of(RepositoryError(errorObject: e));
     }
-    return Option<RepositoryError>.none();
+    return Option.none();
   }
 
 }
