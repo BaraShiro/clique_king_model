5f4d51a8a0aa804e1b4d1d80d27d2b8e52ced08e
Robert Rosborg <rosborg.robert@gmail.com>
2023-10-22 05:15:16 +0200

    Clique repository some work done

--- lib/src/repositories/clique_repository.dart
+++ lib/src/repositories/clique_repository.dart
@@ -6,13 +6,60 @@ import 'package:clique_king_model/clique_king_model.dart';
 
 @immutable
 class CliqueRepository {
-  final Firestore store; // pass it in so it can be mocked.
+  final Firestore store;
 
   CliqueRepository({required this.store});
 
-  // TODO: Create methods for managing cliques stored in Firebase Firestore.
+  Future<Either<RepositoryError, Clique>> createClique({required String name}) async {
+    Clique clique = Clique(name: name);
+    Document document;
+    try {
+      document = await store.collection(cliqueCollection).document(clique.id).create(clique.toMap());
+    } catch (e) {
+      return Either.left(RepositoryError(errorObject: e));
+    }
+    return Either.right(Clique.fromMap(document.map));
+  }
 
-  // Basically, crud.
-  // With Firestore, you have the option to return data once, or return a stream which emits
-  // data anytime data in the query changes.
+  Stream<List<Clique>> readAllCliques() async* {
+    Stream<List<Document>> cliquesStream = store.collection("cliques").stream;
+
+    await for (List<Document> cliquesEvent in cliquesStream) {
+      List<Clique> allCliques = cliquesEvent.map(
+              (cliqueDocument) => Clique.fromMap(cliqueDocument.map)
+      ).toList();
+      yield allCliques;
+    }
+  }
+
+  Stream<List<Score>> readScoresFromClique({required CliqueId cliqueId}) async* {
+    Stream<List<Document>> scoresStream = store.collection(cliqueCollection).document(cliqueId).collection(participantCollection).stream;
+
+    await for (List<Document> scoresEvent in scoresStream) {
+      List<Score> allScores = scoresEvent.map(
+              (scoreDocument) => Score.fromMap(scoreDocument.map)
+      ).toList();
+      yield allScores;
+    }
+  }
+
+  Future<Option<RepositoryError>> addUser({required CliqueId cliqueId, required User user}) async {
+    Score score = Score.fromUser(user);
+    try {
+      await store.collection(cliqueCollection).document(cliqueId).collection(participantCollection).document(score.userId).create(score.toMap());
+    } catch (e) {
+      return Option.of(RepositoryError(errorObject: e));
+    }
+
+    return Option.none();
+  }
+
+  void removeUser({required CliqueId cliqueId, required UserId userId}) async {
+    throw UnimplementedError();
+  }
+
+  void deleteClique({required CliqueId cliqueId}) async {
+    throw UnimplementedError();
+  }
+
 }
